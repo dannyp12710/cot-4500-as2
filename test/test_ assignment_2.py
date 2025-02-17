ad3fe6cb59a0f0a8a595aa57a6d511c2411533cd
dannyp12710 <danielpinilla280@gmail.com>
2025-02-16 19:03:09 -0500

    Update test_ assignment_2.py

--- test/test_ assignment_2.py
+++ test/test_ assignment_2.py
@@ -1,1 +1,40 @@
+# For Question 1 Using Neville’s method, find the 2nd degree interpolating value for f(3.7) for the following
 
+#set of data a.
+# x: 3.6  3.8  3.9 
+# f(x): 1.675 1.436 1.318
+
+
+# For Question 2 Using Newton’s forward method, print out the polynomial approximations for degrees 1, 2, and 3 using the following set of data
+
+# a. Hint, create the table first
+# b.
+# x: 7.2 7.4  7.5  7.6
+# f(x): 23.5492 25.3913 26.8224 27.4589
+
+
+# For question 3 Using the results from Question 2, approximate f(7.3)?
+
+# Solving for f(3.7) 
+# Use data and answers from Question 2 
+# x: 7.2 7.4  7.5  7.6
+# f(x): 23.5492 25.3913 26.8224 27.4589
+# Newton's Forward Coefficients: 1.842100000000002 -0.4110000000000049 -0.3835999999999906
+
+
+
+# For Question 4 Using the divided difference method, print out the Hermite polynomial approximation matrix
+
+# x: 3.6 3.8 3.9 
+# f(x): 1.675 1.436 1.318 
+# f’(x): -1.195 -1.188 -1.182
+
+
+# For Question 5 Using cubic spline interpolation, solve for the following using this set of data:
+
+# x: 2 5 8 10 
+# f(x): 3 5 7 9
+
+# a. Find matrix A
+# b. Vector b
+# c. Vector x
